f04664e60389bc2a75948489b6cc786ba97ac8b0
finn-01 <nguyensam576@gmail.com>
2022-04-14 02:36:11 +0700

    post

--- src/main/java/com/example/employeesystemapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeesystemapi/controller/EmployeeController.java
@@ -3,11 +3,9 @@ package com.example.employeesystemapi.controller;
 import com.example.employeesystemapi.model.Employee;
 import com.example.employeesystemapi.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
